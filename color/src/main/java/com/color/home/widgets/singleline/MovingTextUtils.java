1d7b63e1c6608e0d7d88c112df082ad8fec7eb5b
HUANG Menghuai <moore@lednets.com>
2016-08-27 16:15:31 +0800

    Bumpt to 1.25.318. Donot make cpu performance even it's playing scrolling texts.
    Change-Id: I5f327a9dc21c13691e29e9d4aeaca9d418ec92bf

--- color/src/main/java/com/color/home/widgets/singleline/MovingTextUtils.java
+++ color/src/main/java/com/color/home/widgets/singleline/MovingTextUtils.java
@@ -14,18 +14,8 @@ public class MovingTextUtils {
     public static boolean sIsPerformance = false;
 
     public static float getPixelPerFrame(Item item) {
-        if (!sIsPerformance) {
-
-            ArrayList<String> cmd = new ArrayList<String>();
-            cmd.add(" echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor \n");
-            try {
-                FtpServer.RunAsRoot(cmd.toArray(new String[0])).getInputStream();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            sIsPerformance = true;
-        }
+        // TODO: Double check whether we really need the performance switch.
+        // ensureSystemPerformance();
 
         float pixelPerFrame = 0.2f;
         try {
@@ -52,6 +42,21 @@ public class MovingTextUtils {
 
         return pixelPerFrame;
 
+    }
+
+    public static void ensureSystemPerformance() {
+        if (!sIsPerformance) {
+
+            ArrayList<String> cmd = new ArrayList<String>();
+            cmd.add(" echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor \n");
+            try {
+                FtpServer.RunAsRoot(cmd.toArray(new String[0])).getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            sIsPerformance = true;
+        }
     }
 
     public static int evenIt(int dimension) {
